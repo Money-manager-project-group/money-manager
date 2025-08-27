7a84850365c5d9e77b3af0d5b9f8cef2e79dcb8e
Jinwook Kim <wlsdnr710@naver.com>
2025-08-27 12:51:55 +0900

    slight change style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,12 +26,12 @@ const NavBar = () => {
 
   const imgClass = "w-7 inline-block mr-2 group-hover:invert-100";
   const liClass =
-    "rounded-sm font-sans px-4 py-2 flex justify-center w-fit h-fit hover:bg-gray-500 hover:text-white group";
+    "rounded-sm font-sans px-5 py-2 flex justify-center w-fit h-fit hover:bg-gray-500 hover:text-white group";
   const selectedImgClass = imgClass + " invert-100";
   const selectedLiClass = liClass + " bg-black text-white";
   return (
     <nav className="bg-white shadow-md text-[2vw] px-6 py-2 flex ">
-      <ul className="flex space-x-8 ">
+      <ul className="flex space-x-8 text-2xl font-semibold">
         <li className={highlightPage == "home" ? selectedLiClass : liClass}>
           <NavLink to="/" end>
             <img
